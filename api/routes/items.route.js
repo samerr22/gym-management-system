778a583fab61cafd562cb2074772433a1f9b9e6d
sameeera <laksameera5@gmail.com>
2024-09-12 09:37:26 +0530

    add new tow route

--- api/routes/items.route.js
+++ api/routes/items.route.js
@@ -1,10 +1,12 @@
 import  express  from "express";
-import { Cartcrete, deleteItems, deleteItemss, getAllItems, getCartItem,Itcreate } from "../controllers/items.controller.js";
+import { Cartcrete, deleteItem, deleteItems, deleteItemss, getAllItems, getCartItem,Itcreate, updateItem } from "../controllers/items.controller.js";
 
 const router = express.Router();
 
 router.post('/Icreate',Itcreate );
 router.get('/IgetAll', getAllItems);
+router.put('/Update/:itemId', updateItem);
+router.get('/delete/:ItemmId', deleteItem);
 router.post('/Ccreate',Cartcrete );
 router.get('/CgetAll/:CurrentuserId', getCartItem);
 router.delete('/deletes/:itemsId',deleteItems);
